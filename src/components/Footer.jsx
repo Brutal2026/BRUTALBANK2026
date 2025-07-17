5eb1c860946d2b7026aa7a7d2862c673a2cc4522
Brutal2026 <soumon.aibrasil@gmail.com>
2025-07-17 19:42:45 -0300

    Completely remove logo reference from Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,5 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import logo from '../assets/logo.jpg'//... outras linhas de código ...
-{/* <img src={logo} alt="Logo" className="h-8" /> */}
 
 const Footer = () => {
   return (
